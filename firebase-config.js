b4f9dd64a51b8d978313993f547d315975bbf2bd
Historia Nova Token <rkaya57.77@gmail.com>
2026-03-29 07:29:51 +0300

    Export firebaseConfig for external use

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAE5a-0MmLW0zkuS3ya1veQZvTnyNIbzV4",
   authDomain: "hive-c5d27.firebaseapp.com",
   projectId: "hive-c5d27",
@@ -6,4 +6,4 @@ const firebaseConfig = {
   messagingSenderId: "1015325344229",
   appId: "1:1015325344229:web:803734ef6a386169cbaf0c",
   measurementId: "G-TGBFWY2LNY"
-};
+};;
